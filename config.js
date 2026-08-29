3970269306aeba3b05bf51b441f71dc9fa42e713
Simonchuelo <prueba404100@gmail.com>
2026-08-29 17:40:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // 1) API_URL: la URL que te da Google Apps Script
 //    al desplegar como "Web app" (termina en /exec)
 // ============================================
-var API_URL = 'https://script.google.com/macros/s/AKfycbxH4nR4Ke0xXnGAL0pmcVoMRKTAN5mY9c-7JxYNnNYgGWzrXIEIKkjQvt1uaRn23pZt/exec'
+var API_URL = 'https://script.google.com/macros/s/AKfycbyHVTbFIyeoCnGeHCMcvTaByXKz7cm2LbgjPY9cvEiYWOcdM_Foq8mACeB71Qeu0RcG/exec'
 
 // Telefono de WhatsApp (con codigo de pais, sin +, sin espacios)
 var WPP_ADMIN = '5491164673729';
